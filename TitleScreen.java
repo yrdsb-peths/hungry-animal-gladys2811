53cf720c8226142f68631cc577f2a07ac34ca658
gladys2811 <348629502@gapps.yrdsb.ca>
2022-12-13 23:08:02 -0500

    Made a title screen

---
+++ TitleScreen.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title Screen 
+ * @author Gladys 
+ * @version December 2022
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label ("Hungry Elephant", 100);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    /**
+     * Main world act loop
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
